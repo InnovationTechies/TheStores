77e9895529fda5e48b6ac22e84d310e54707d4f5
Lutho <lutho.ntlabathi@gmail.com>
2018-08-02 16:56:21 +0200

    Manager adding woolworth

---
+++ WOOLWORTH/Models/WoolworthProductCatergory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WOOLWORTH.Models
+{
+    public class WoolworthProductCatergory
+    {
+        [Key]
+        public int catagoryID { get; set; }
+        public string catagoryName { get; set; }
+        public string catagoryDesc { get; set; }
+    }
+}
