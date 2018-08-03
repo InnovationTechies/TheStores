fdc4ddb1f8e262caa8557dfe3fc846dc0717fc86
songeziwe <nciwenisongeziwe@gmail.com>
2018-08-03 10:08:44 +0200

    Created controllers

---
+++ CHECKERS/Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHECKERS.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int catagoryID { get; set; }
+        public string catagoryName { get; set; }
+        public string catagoryDesc { get; set; }
+    }
+}
---
+++ CHECKERS/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHECKERS.Models
+{
+    public class Products
+    {
+        [Key]
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string productImage { get; set; }
+        public double productDropPercent { get; set; }
+        public string productDesc { get; set; }
+        public DateTime productDateEndPromo { get; set; }
+        public IEnumerable<ProductCatagory> productCatagories { get; set; }
+    }
+}
