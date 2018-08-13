4269fd8791603857206109afcece4b16f7a59e79
Lutho <lutho.ntlabathi@gmail.com>
2018-08-13 16:37:05 +0200

    lutho  monday 13/08/2018

--- WOOLWORTH/Controllers/WoolworthProductsController.cs
+++ WOOLWORTH/Controllers/WoolworthProductsController.cs
@@ -46,7 +46,7 @@ namespace WOOLWORTH.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "productID,productName,productImage,productDropPercent,productDesc,productDateEndPromo")] WoolworthProducts woolworthProducts)
+        public ActionResult Create([Bind(Include = "productID,productName,productImage,ProductPrice,productDropPercent,productDesc,productDateEndPromo")] WoolworthProducts woolworthProducts)
         {
             if (ModelState.IsValid)
             {
