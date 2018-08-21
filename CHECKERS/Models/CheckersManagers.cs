a8eaabd847028f93a5ef6ee4f57787ba6a0e46c5
songeziwe <nciwenisongeziwe@gmail.com>
2018-08-21 11:25:33 +0200

    Models

---
+++ CHECKERS/Models/CheckersManagers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CHECKERS.Models
+{
+    public class CheckersManagers
+    {
+        [Key]
+        public int CheckersManID { get; set; }
+        [Display(Name = "Name")]
+        public string CheckersManName { get; set; }
+        [Display(Name = "Surname")]
+        public string CheckersManSurname { get; set; }
+        [Display(Name = "Email")]
+        public string CheckersManEmail { get; set; }
+        [Display(Name = "Password")]
+        public string CheckersManPassword { get; set; }
+
+        public ICollection<CheckersSales> Sales { get; set; }
+    }
+}
---
+++ CHECKERS/Models/CheckersSpecials.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CHECKERS.Models
+{
+    public class CheckersSpecials
+    {
+        [Key]
+        public int CheckersSpecialID { get; set; }
+        [Display(Name = "Name")]
+        public string CheckersSpecialName { get; set; }
+        [Display(Name = "Description")]
+        public string CheckersSpecialDesc { get; set; }
+        [Display(Name = "StartDate")]
+        public DateTime CheckersStartDate { get; set; }
+        [Display(Name = "EndDate")]
+        public DateTime CheckersEndDate { get; set; }
+
+        //public ICollection<PnpSales> Sales { get; set; }
+    }
+}
