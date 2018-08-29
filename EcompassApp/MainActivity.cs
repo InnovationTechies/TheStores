2cb7ad35d233dd48eca52390883c960e6bc5fad8
Cwenga Zozo <cwengazozo@outlook.com>
2018-08-29 16:48:27 +0200

    Wednesday

--- EcompassApp/MainActivity.cs
+++ EcompassApp/MainActivity.cs
@@ -3,10 +3,11 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
-using System.ServiceModel;
-
-using EcompassServiceProxy.EcompassServiceProxy;
+using System.Net.Http;
 using System;
+using System.Collections.Generic;
+using ServiceProxy.EcompassServiceProxy;
+using System.ServiceModel;
 
 namespace EcompassApp
 {
@@ -15,10 +16,8 @@ namespace EcompassApp
     {
         static readonly EndpointAddress Endpoint = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/StoresService/EcompassService/");
 
-        /// <summary>
-        /// get service references
-        /// </summary>
-        //EcompassServiceClient _client;
+
+        EcompassServiceClient _client;
         Button _getHelloWorldDataButton;
         TextView _getHelloWorldDataTextView;
         Button _sayHelloWorldButton;
@@ -34,34 +33,38 @@ namespace EcompassApp
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_home);
+            SetContentView(Resource.Layout.activity_main);
 
             btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
             txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
             txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
 
-            btnLogin.Click += btnLogin_Click;
+           // btnLogin.Click += btnLogin_Click;
 
 
             InitializeEcompassServiceClient();
 
             // This button will invoke the GetHelloWorldData - the method that takes a C# object as a parameter.
-            _getHelloWorldDataButton = FindViewById<Button>(Resource.Id.get_hello_products_data);
-           // _getHelloWorldDataButton.Click += GetHelloWorldDataButtonOnClick;
-            _getHelloWorldDataTextView = FindViewById<TextView>(Resource.Id.listProducts);
+            _getHelloWorldDataButton = FindViewById<Button>(Resource.Id.getHelloWorldDataButton);
+            _getHelloWorldDataButton.Click += GetHelloWorldDataButtonOnClick;
+            _getHelloWorldDataTextView = FindViewById<TextView>(Resource.Id.getHelloWorldDataTextView);
+
+
 
             // This button will invoke SayHelloWorld - this method takes a simple string as a parameter.
-            _sayHelloWorldButton = FindViewById<Button>(Resource.Id.say_hello_ecompass);
-            //_sayHelloWorldButton.Click += SayHelloWorldButtonOnClick;
-            _sayHelloWorldTextView = FindViewById<TextView>(Resource.Id.sayHelloTextView);
+            _sayHelloWorldButton = FindViewById<Button>(Resource.Id.sayHelloWorldButton);
+            _sayHelloWorldButton.Click += SayHelloWorldButtonOnClick;
+            _sayHelloWorldTextView = FindViewById<TextView>(Resource.Id.sayHelloWorldTextView);
+        
 
-        }
+    }
 
         void InitializeEcompassServiceClient()
         {
             BasicHttpBinding binding = CreateBasicHttpBinding();
-            //_client = new EcompassServiceClient(binding, Endpoint);
+            _client = new EcompassServiceClient(binding, Endpoint);
         }
+
 
         static BasicHttpBinding CreateBasicHttpBinding()
         {
@@ -89,6 +92,42 @@ namespace EcompassApp
             else
             {
                 Toast.MakeText(this, "üsername or password is incorrect", ToastLength.Long).Show();
+            }
+        }
+
+
+        async void GetHelloWorldDataButtonOnClick(object sender, EventArgs e)
+        {
+            var data = new List<PnpProducts>
+            {
+                //Name = "Mr. Chad",
+                //SayHello = true
+            };
+
+            _getHelloWorldDataTextView.Text = "Waiting for WCF...";
+            PnpProducts result;
+            try
+            {
+                //result = await _client.GetProductsData();
+                //_getHelloWorldDataTextView.Text = result.ProductName;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        async void SayHelloWorldButtonOnClick(object sender, EventArgs e)
+        {
+            _sayHelloWorldTextView.Text = "Waiting for WCF...";
+            try
+            {
+                //var result = await _client.SayHelloToAsync("Kilroy");
+                //_sayHelloWorldTextView.Text = result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
 
